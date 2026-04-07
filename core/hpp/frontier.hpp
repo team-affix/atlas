62e59a5e26afe6171645e8f1d7eed3f3c341979f
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2026-04-06 18:35:33 -0700

    Made frontier backtrackable

--- core/hpp/frontier.hpp
+++ core/hpp/frontier.hpp
@@ -9,17 +9,15 @@ struct frontier {
     virtual ~frontier() = default;
     frontier(
         const database&,
-        lineage_pool&
+        lineage_pool&,
+        trail&
     );
-    void insert(const goal_lineage*, const T&);
+    void upsert(const goal_lineage*, const T&);
     void resolve(const resolution_lineage*);
     virtual std::vector<T> expand(const T&, const rule&) = 0;
 
-    std::unordered_map<const goal_lineage*, T>::iterator begin();
-    std::unordered_map<const goal_lineage*, T>::iterator end();
     std::unordered_map<const goal_lineage*, T>::const_iterator begin() const;
     std::unordered_map<const goal_lineage*, T>::const_iterator end() const;
-    T& at(const goal_lineage*);
     const T& at(const goal_lineage*) const;
     size_t size() const;
     bool empty() const;
@@ -28,6 +26,7 @@ private:
 #endif
     const database& db;
     lineage_pool& lp;
+    trail& t;
 
     std::unordered_map<const goal_lineage*, T> members;
 };
@@ -35,11 +34,26 @@ private:
 template<typename T>
 frontier<T>::frontier(
     const database& db,
-    lineage_pool& lp) : db(db), lp(lp) {}
+    lineage_pool& lp,
+    trail& t) : db(db), lp(lp), t(t) {}
 
 template<typename T>
-void frontier<T>::insert(const goal_lineage* gl, const T& value) {
-    members.insert({gl, value});
+void frontier<T>::upsert(const goal_lineage* gl, const T& value) {
+    auto [it, inserted] = members.insert({gl, value});
+
+    if (inserted) {
+        t.log(
+            [this, gl]{members.erase(gl);},
+            [this, gl, value]{members.insert({gl, value});}
+        );
+    }
+    else {
+        t.log(
+            [this, gl, old = it->second]{members.at(gl) = old;},
+            [this, gl, value]{members.at(gl) = value;}
+        );
+        it->second = value;
+    }
 }
 
 template<typename T>
@@ -53,22 +67,18 @@ void frontier<T>::resolve(const resolution_lineage* r) {
     // expand the parent's value with the rule at the resolution index
     auto child_values = expand(parent_value, db.at(r->idx));
 
+    // log the erasure of the parent
+    t.log(
+        [this, parent, val = parent_value]{members.insert({parent, val});},
+        [this, parent]{members.erase(parent);}
+    );
+
     // erase the parent from the frontier
     members.erase(parent);
     
     // add the children to the frontier
     for (int i = 0; i < child_values.size(); i++)
-        members.insert({lp.goal(r, i), child_values[i]});
-}
-
-template<typename T>
-std::unordered_map<const goal_lineage*, T>::iterator frontier<T>::begin() {
-    return members.begin();
-}
-
-template<typename T>
-std::unordered_map<const goal_lineage*, T>::iterator frontier<T>::end() {
-    return members.end();
+        upsert(lp.goal(r, i), child_values[i]);
 }
 
 template<typename T>
@@ -79,11 +89,6 @@ std::unordered_map<const goal_lineage*, T>::const_iterator frontier<T>::begin() 
 template<typename T>
 std::unordered_map<const goal_lineage*, T>::const_iterator frontier<T>::end() const {
     return members.end();
-}
-
-template<typename T>
-T& frontier<T>::at(const goal_lineage* gl) {
-    return members.at(gl);
 }
 
 template<typename T>
