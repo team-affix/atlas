3724b7a42d393e845fe8eadad4d9170961e63b4c
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2026-04-25 00:39:58 -0700

    Added goal_expander class

---
+++ core/cpp/goal_expander.cpp
@@ -0,0 +1,26 @@
+#include <stdexcept>
+#include "../hpp/goal_expander.hpp"
+#include "../hpp/bind_map.hpp"
+#include "../hpp/copier.hpp"
+
+goal_expander::goal_expander(
+    const expr*& goal,
+    const rule& r,
+    copier& cp,
+    bind_map& bm) : cp(cp), subgoal_index(0) {
+
+    // copy the head of the rule
+    const expr* copied_head = cp(r.head, translation_map);
+
+    // unify the goal with the head of the rule
+    if (!bm.unify(goal, copied_head))
+        throw std::runtime_error("Failed to unify the goal with the head of the rule");
+
+    // save the original body
+    rule_body = r.body;
+}
+
+const expr* goal_expander::operator()() {
+    // copy the member of the rule body at the subgoal index
+    return cp(rule_body.at(subgoal_index++), translation_map);
+}
---
+++ core/hpp/goal_expander.hpp
@@ -0,0 +1,27 @@
+#ifndef GOAL_EXPANDER_HPP
+#define GOAL_EXPANDER_HPP
+
+#include <map>
+#include "expr.hpp"
+#include "rule.hpp"
+#include "copier.hpp"
+#include "bind_map.hpp"
+
+struct goal_expander {
+    goal_expander(
+        const expr*& goal,
+        const rule& r,
+        copier& cp,
+        bind_map& bm);
+    const expr* operator()();
+#ifndef DEBUG
+private:
+#endif
+    copier& cp;
+
+    std::map<uint32_t, uint32_t> translation_map;
+    std::vector<const expr*> rule_body;
+    size_t subgoal_index;
+};
+
+#endif
