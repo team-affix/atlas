25977af6261b51b580e020a681a72ce85afc1a72
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2026-04-26 10:12:09 -0700

    Updated the locator to use types as the key

--- core/cpp/locator.cpp
+++ core/cpp/locator.cpp
@@ -1,17 +1,18 @@
-#include "../hpp/locator.hpp"
-
-void locator::unbind(locator_keys key) {
-    entries.erase(key);
-}
+#include "../hpp/infrastructure/locator.hpp"
 
 void locator::push_frame() {
-    past_frames.push(current_frame_additions);
+    past_frames.push(std::move(current_frame_additions));
     current_frame_additions.clear();
 }
 
 void locator::pop_frame() {
-    for (auto key : current_frame_additions)
-        unbind(key);
-    current_frame_additions = past_frames.top();
+    clear_frame();
+    current_frame_additions = std::move(past_frames.top());
     past_frames.pop();
+}
+
+void locator::clear_frame() {
+    for (auto type : current_frame_additions)
+        entries.erase(type);
+    current_frame_additions.clear();
 }
--- core/hpp/infrastructure/locator.hpp
+++ core/hpp/infrastructure/locator.hpp
@@ -5,64 +5,35 @@
 #include <cassert>
 #include <stack>
 #include <unordered_set>
-
-enum class locator_keys {
-    inst_database,
-    inst_goals,
-    inst_trail,
-    inst_var_sequencer,
-    inst_bind_map,
-    inst_normalizer,
-    inst_expr_printer,
-    inst_expr_pool,
-    inst_goal_store,
-    inst_candidate_store,
-    inst_copier,
-    inst_cdcl,
-    inst_cdcl_eliminator,
-    inst_head_eliminator,
-    inst_frontier_watch,
-    inst_resolution_lineage,
-    inst_goal_lineage,
-    inst_lineage_pool,
-    inst_rep_changed_topic,
-    inst_goal_inserted_topic,
-    inst_goal_resolved_topic,
-    inst_new_eliminated_resolution_topic,
-    inst_unit_topic,
-    inst_mcts_decider,
-    inst_mcts_sim,
-    inst_mcts_exploration_constant,
-    inst_mcts_rng,
-    inst_max_resolutions
-};
+#include <typeindex>
 
 struct locator {
     template<typename T>
-    static T& locate(locator_keys);
+    static T& locate();
     template<typename T>
-    static void bind(locator_keys, T& value);
-    static void unbind(locator_keys);
+    static void bind(T& value);
     static void push_frame();
     static void pop_frame();
+    static void clear_frame();
 #ifndef DEBUG
 private:
 #endif
-    static std::unordered_map<locator_keys, void*> entries;
-    static std::unordered_set<locator_keys> current_frame_additions;
-    static std::stack<std::unordered_set<locator_keys>> past_frames;
+    static std::unordered_map<std::type_index, void*> entries;
+    static std::unordered_set<std::type_index> current_frame_additions;
+    static std::stack<std::unordered_set<std::type_index>> past_frames;
 };
 
 template<typename T>
-T& locator::locate(locator_keys key) {
-    return *reinterpret_cast<T*>(entries.at(key));
+T& locator::locate() {
+    return *reinterpret_cast<T*>(entries.at(std::type_index(typeid(T))));
 }
 
 template<typename T>
-void locator::bind(locator_keys key, T& value) {
-    auto [_, success] = entries.insert({key, &value});
+void locator::bind(T& value) {
+    std::type_index type{typeid(T)};
+    auto [_, success] = entries.insert({type, &value});
     assert(success);
-    current_frame_additions.insert(key);
+    current_frame_additions.insert(type);
 }
 
 #endif
