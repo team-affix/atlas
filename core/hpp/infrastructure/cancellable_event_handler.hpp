b4d88f88919564dbf7562f26a7a97273e4ae6e40
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2026-04-27 20:15:01 -0700

    implemented cancellable_event_handler

---
+++ core/hpp/infrastructure/cancellable_event_handler.hpp
@@ -0,0 +1,33 @@
+#ifndef CANCELLABLE_EVENT_HANDLER_HPP
+#define CANCELLABLE_EVENT_HANDLER_HPP
+
+#include "event_handler.hpp"
+#include "cancellation_token.hpp"
+#include "../bootstrap/resolver.hpp"
+
+template<typename Event>
+struct cancellable_event_handler : event_handler<Event> {
+    virtual ~cancellable_event_handler() = default;
+    cancellable_event_handler();
+    virtual void execute() = 0;
+#ifndef DEBUG
+private:
+#endif
+    void operator()(const Event&) final override;
+    const cancellation_token& token;
+};
+
+template<typename Event>
+cancellable_event_handler<Event>::cancellable_event_handler() :
+    event_handler<Event>(),
+    token(resolver::resolve<cancellation_token>()) {
+}
+
+template<typename Event>
+void cancellable_event_handler<Event>::operator()(const Event& event) {
+    if (token.is_cancelled())
+        return;
+    execute();
+}
+
+#endif
