4002edc594347785a4047e967a375818f1bf80d7
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2026-05-01 17:22:30 -0700

    Implemented cdcl_sequencer.cpp

--- core/cpp/infrastructure/cdcl_sequencer.cpp
+++ core/cpp/infrastructure/cdcl_sequencer.cpp
@@ -1,6 +1,9 @@
 #include "../../hpp/infrastructure/cdcl_sequencer.hpp"
+#include "../../hpp/bootstrap/resolver.hpp"
 
-cdcl_sequencer::cdcl_sequencer() : seq() {}
+cdcl_sequencer::cdcl_sequencer() :
+    seq(resolver::resolve<i_trail>()) {
+}
 
 size_t cdcl_sequencer::next() {
     return seq.next();
