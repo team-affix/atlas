4cc82e3dcebd78a2f850daf11385564bef9e96ac
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2026-05-01 21:38:58 -0700

    Added goal_candidates_expander

---
+++ core/cpp/domain/entities/goal_candidates_expander.cpp
@@ -0,0 +1,19 @@
+#include "../../../hpp/domain/entities/goal_candidates_expander.hpp"
+#include "../../../hpp/bootstrap/resolver.hpp"
+
+goal_candidates_expander::goal_candidates_expander()
+    :
+    db(resolver::resolve<i_database>()),
+    gcs(resolver::resolve<i_goal_candidates_store>()),
+    goal_candidates_changed_producer(resolver::resolve<i_event_producer<goal_candidates_changed_event>>()) {
+    for (size_t i = 0; i < db.size(); ++i)
+        initial_candidates.insert(i);
+}
+
+void goal_candidates_expander::start_expansion(const resolution_lineage* rl) {
+}
+
+void goal_candidates_expander::expand_child(const goal_lineage* gl) {
+    gcs.insert(gl, candidate_set{initial_candidates});
+    goal_candidates_changed_producer.produce(goal_candidates_changed_event{gl});
+}
---
+++ core/hpp/domain/entities/goal_candidates_expander.hpp
@@ -0,0 +1,22 @@
+#ifndef GOAL_CANDIDATES_EXPANDER_HPP
+#define GOAL_CANDIDATES_EXPANDER_HPP
+
+#include "../interfaces/i_goal_candidates_expander.hpp"
+#include "../interfaces/i_database.hpp"
+#include "../interfaces/i_goal_candidates_store.hpp"
+#include "../interfaces/i_event_producer.hpp"
+#include "../events/goal_candidates_changed_event.hpp"
+
+struct goal_candidates_expander : i_goal_candidates_expander {
+    goal_candidates_expander();
+    void start_expansion(const resolution_lineage*) override;
+    void expand_child(const goal_lineage*) override;
+private:
+    i_database& db;
+    i_goal_candidates_store& gcs;
+    i_event_producer<goal_candidates_changed_event>& goal_candidates_changed_producer;
+
+    std::unordered_set<size_t> initial_candidates;
+};
+
+#endif
