0b9a3c295ac7ccd86b293e5264c24e96a079daf6
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2026-05-16 03:05:29 -0700

    Removed unnecessary header

--- core/hpp/interfaces/i_cdcl_elimination_generator.hpp
+++ core/hpp/interfaces/i_cdcl_elimination_generator.hpp
@@ -4,7 +4,6 @@
 #include "i_elimination_generator.hpp"
 #include "../value_objects/lineage.hpp"
 #include "../value_objects/lemma.hpp"
-#include "../utility/state_machine.hpp"
 
 struct i_cdcl_elimination_generator : i_elimination_generator {
     virtual ~i_cdcl_elimination_generator() = default;
