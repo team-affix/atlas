3eba941989baaf0fa9ffefff6de9bb50235ba429
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2026-05-18 12:57:10 -0700

    updated functionality of elimination_router

--- core/cpp/infrastructure/elimination_router.cpp
+++ core/cpp/infrastructure/elimination_router.cpp
@@ -4,19 +4,20 @@ elimination_router::elimination_router(
     i_deactivated_candidate_memory& dcm,
     i_active_goals& ag,
     i_elimination_backlog& eb,
-    i_active_candidate_eliminator& ace)
-    : dcm(dcm), ag(ag), eb(eb), ace(ace) {
+    i_candidate_deactivator& cd)
+    : dcm(dcm), ag(ag), eb(eb), cd(cd) {
 }
 
 elimination_result elimination_router::route(const resolution_lineage* rl) {
     // 1. check if candidate is deactivated
     if (dcm.contains(rl))
-        return already_deactivated{};
+        return elimination_result::already_deactivated;
     // 2. check if candidate is not yet active
     if (!ag.contains(rl->parent)) {
         eb.insert(rl);
-        return added_to_backlog{};
+        return elimination_result::added_to_backlog;
     }
-    // 3. the candidate is active, eliminate it
-    return ace.eliminate(rl);
+    // 3. the candidate is active, deactivate it
+    cd.deactivate(rl);
+    return elimination_result::eliminated;
 }
--- core/hpp/infrastructure/elimination_router.hpp
+++ core/hpp/infrastructure/elimination_router.hpp
@@ -5,20 +5,20 @@
 #include "../interfaces/i_deactivated_candidate_memory.hpp"
 #include "../interfaces/i_active_goals.hpp"
 #include "../interfaces/i_elimination_backlog.hpp"
-#include "../interfaces/i_active_candidate_eliminator.hpp"
+#include "../interfaces/i_candidate_deactivator.hpp"
 
 struct elimination_router : i_elimination_router {
     elimination_router(
         i_deactivated_candidate_memory& dcm,
         i_active_goals& ag,
         i_elimination_backlog& eb,
-        i_active_candidate_eliminator& ace);
+        i_candidate_deactivator& cd);
     elimination_result route(const resolution_lineage*) override;
 private:
     i_deactivated_candidate_memory& dcm;
     i_active_goals& ag;
     i_elimination_backlog& eb;
-    i_active_candidate_eliminator& ace;
+    i_candidate_deactivator& cd;
 };
 
 #endif
