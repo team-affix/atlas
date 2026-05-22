77be70c6a6802439fbe79e41d12d7fc1721cbaa9
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2026-05-22 00:25:30 -0700

    vetted sequencer tests

--- core/test/integration/sequencer.cpp
+++ core/test/integration/sequencer.cpp
@@ -9,25 +9,25 @@ protected:
 };
 
 TEST_F(SequencerIntegrationTest, PopRevertsCounter) {
-    seq.next();
-    seq.next();
+    EXPECT_EQ(seq.next(), 0);
+    EXPECT_EQ(seq.next(), 1);
     t.push();
-    seq.next();
-    seq.next();
-    seq.next();
+    EXPECT_EQ(seq.next(), 2);
+    EXPECT_EQ(seq.next(), 3);
+    EXPECT_EQ(seq.next(), 4);
     t.pop();
     EXPECT_EQ(seq.next(), 2);
 }
 
 TEST_F(SequencerIntegrationTest, TwoSequencersSharingTrailRevertIndependently) {
     sequencer<int> seq2{t};
-    seq.next();
-    seq.next();
-    seq2.next();
+    EXPECT_EQ(seq.next(), 0);
+    EXPECT_EQ(seq.next(), 1);
+    EXPECT_EQ(seq2.next(), 0);
     t.push();
-    seq.next();
-    seq2.next();
-    seq2.next();
+    EXPECT_EQ(seq.next(), 2);
+    EXPECT_EQ(seq2.next(), 1);
+    EXPECT_EQ(seq2.next(), 2);
     t.pop();
     EXPECT_EQ(seq.next(), 2);
     EXPECT_EQ(seq2.next(), 1);
