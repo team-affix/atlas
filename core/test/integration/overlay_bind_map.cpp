aafe892a35baee7b687247af5a08a24ee2ee05b9
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2026-05-22 00:23:02 -0700

    Updated and vetted tests in overlay_bind_map

--- core/test/integration/overlay_bind_map.cpp
+++ core/test/integration/overlay_bind_map.cpp
@@ -7,16 +7,16 @@ protected:
     bind_map local;
     bind_map remote;
     overlay_bind_map obm{local, remote};
-    i_bind_map& sut{obm};
 
     expr var0{expr::var{0}};
+    expr var1{expr::var{1}};
     expr func{expr::functor{"f", {}}};
     expr func2{expr::functor{"g", {}}};
     expr func_of_var0{expr::functor{"f", {&var0}}};
 };
 
 TEST_F(OverlayBindMapIntegrationTest, BindWritesOnlyToLocalLayer) {
-    sut.bind(0, &func);
+    obm.bind(0, &func);
 
     EXPECT_EQ(local.whnf(&var0), &func);
     EXPECT_EQ(remote.whnf(&var0), &var0);
@@ -25,27 +25,27 @@ TEST_F(OverlayBindMapIntegrationTest, BindWritesOnlyToLocalLayer) {
 TEST_F(OverlayBindMapIntegrationTest, WhnfUsesLocalBindingWhenPresent) {
     local.bind(0, &func);
 
-    EXPECT_EQ(sut.whnf(&var0), &func);
+    EXPECT_EQ(obm.whnf(&var0), &func);
     EXPECT_EQ(remote.whnf(&var0), &var0);
 }
 
 TEST_F(OverlayBindMapIntegrationTest, WhnfFallsThroughToRemoteWhenLocalUnbound) {
     remote.bind(0, &func);
 
-    EXPECT_EQ(sut.whnf(&var0), &func);
+    EXPECT_EQ(obm.whnf(&var0), &func);
 }
 
 TEST_F(OverlayBindMapIntegrationTest, WhnfLocalShadowsRemote) {
     local.bind(0, &func);
-    remote.bind(0, &func2);
+    remote.bind(0, &var1);
 
-    EXPECT_EQ(sut.whnf(&var0), &func);
+    EXPECT_EQ(obm.whnf(&var0), &func);
 }
 
 TEST_F(OverlayBindMapIntegrationTest, WhnfDoesNotDescendIntoFunctorArguments) {
     remote.bind(0, &func);
 
-    EXPECT_EQ(sut.whnf(&func_of_var0), &func_of_var0);
+    EXPECT_EQ(obm.whnf(&func_of_var0), &func_of_var0);
 }
 
 TEST_F(OverlayBindMapIntegrationTest, TwoOverlaysShareRemoteButHaveIndependentLocals) {
@@ -55,7 +55,9 @@ TEST_F(OverlayBindMapIntegrationTest, TwoOverlaysShareRemoteButHaveIndependentLo
     obm.bind(0, &func);
     obm2.bind(0, &func2);
 
+    EXPECT_EQ(remote.whnf(&var0), &var0);
     EXPECT_EQ(local.whnf(&var0), &func);
     EXPECT_EQ(local2.whnf(&var0), &func2);
-    EXPECT_EQ(remote.whnf(&var0), &var0);
+    EXPECT_EQ(obm.whnf(&var0), &func);
+    EXPECT_EQ(obm2.whnf(&var0), &func2);
 }
