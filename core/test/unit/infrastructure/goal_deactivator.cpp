6a37e018c3a08b8e5642c75c4497a6e137deeb0d
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2026-05-28 17:31:50 -0700

    Fixed intellisense errors

--- core/test/unit/infrastructure/goal_deactivator.cpp
+++ core/test/unit/infrastructure/goal_deactivator.cpp
@@ -2,7 +2,6 @@
 // and removes the goal from the active set.
 
 #include <gtest/gtest.h>
-#include "locator_fixture.hpp"
 #include <gmock/gmock.h>
 #include "infrastructure/goal_deactivator.hpp"
 #include "interfaces/i_unset_goal_expr.hpp"
