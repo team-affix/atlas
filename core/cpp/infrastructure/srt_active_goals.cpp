2d4587b08f6c3e868b88339ccd235d0613282ca6
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2026-06-08 16:45:22 -0700

    Implemented srt_active_goals

--- core/cpp/infrastructure/srt_active_goals.cpp
+++ core/cpp/infrastructure/srt_active_goals.cpp
@@ -1,8 +1,45 @@
 #include "infrastructure/srt_active_goals.hpp"
+#include "debug_assert.hpp"
+
+void srt_active_goals::insert_active_goal(const goal_lineage* gl) {
+    auto [_, inserted] = in_flight_.emplace(gl);
+    DEBUG_ASSERT(inserted);
+    DEBUG_ASSERT(tree_.insert(gl));
+}
+
+void srt_active_goals::link_srt_goal_batch_parent(const goal_lineage* parent) {
+    tree_.link(parent, in_flight_);
+}
+
+void srt_active_goals::flush_srt_goal_batch() {
+    in_flight_.clear();
+}
+
+void srt_active_goals::erase_active_goal(const goal_lineage* gl) {
+    DEBUG_ASSERT(tree_.unlink(gl));
+    DEBUG_ASSERT(tree_.erase(gl));
+}
+
+bool srt_active_goals::is_active_goal(const goal_lineage* gl) const {
+    return tree_.leaves().contains(gl);
+}
+
+size_t srt_active_goals::active_goals_size() const {
+    return tree_.leaves().size();
+}
+
+bool srt_active_goals::empty() const {
+    return tree_.leaves().empty();
+}
+
+void srt_active_goals::clear_active_goals() {
+    tree_.clear();
+    in_flight_.clear();
+}
 
 coroutine<const goal_lineage*, void> srt_active_goals::iterate_root_goals() const {
     for (const goal_lineage* gl : tree_.roots())
-    co_yield gl;
+        co_yield gl;
 }
 
 coroutine<const goal_lineage*, void> srt_active_goals::iterate_child_goals(const goal_lineage* gl) const {
--- core/hpp/infrastructure/srt_active_goals.hpp
+++ core/hpp/infrastructure/srt_active_goals.hpp
@@ -1,6 +1,7 @@
 #ifndef SRT_ACTIVE_GOALS_HPP
 #define SRT_ACTIVE_GOALS_HPP
 
+#include <set>
 #include "infrastructure/series_reduced_tree.hpp"
 #include "interfaces/i_insert_active_goal.hpp"
 #include "interfaces/i_erase_active_goal.hpp"
@@ -10,6 +11,8 @@
 #include "interfaces/i_active_goals_size.hpp"
 #include "interfaces/i_check_active_goals_empty.hpp"
 #include "interfaces/i_clear_active_goals.hpp"
+#include "interfaces/i_srt_link_goal_batch_parent.hpp"
+#include "interfaces/i_srt_flush_goal_batch.hpp"
 
 struct srt_active_goals
     : i_insert_active_goal
@@ -19,17 +22,22 @@ struct srt_active_goals
     , i_iterate_child_goals
     , i_active_goals_size
     , i_check_active_goals_empty
-    , i_clear_active_goals {
+    , i_clear_active_goals
+    , i_srt_link_goal_batch_parent
+    , i_srt_flush_goal_batch {
     void insert_active_goal(const goal_lineage*) override;
     void erase_active_goal(const goal_lineage*) override;
     bool is_active_goal(const goal_lineage*) const override;
-    coroutine<const goal_lineage*, void> iterate_root_goals() const override;
-    coroutine<const goal_lineage*, void> iterate_child_goals(const goal_lineage*) const override;
     size_t active_goals_size() const override;
     bool empty() const override;
     void clear_active_goals() override;
+    void link_srt_goal_batch_parent(const goal_lineage*) override;
+    void flush_srt_goal_batch() override;
+    coroutine<const goal_lineage*, void> iterate_root_goals() const override;
+    coroutine<const goal_lineage*, void> iterate_child_goals(const goal_lineage*) const override;
 private:
     series_reduced_tree<const goal_lineage*> tree_;
+    std::set<const goal_lineage*> in_flight_;
 };
 
 #endif
