7d9417b975172dca59bc7f8a15f6fa5bc80694c8
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2026-07-06 19:06:57 -0700

    added dbuct_nearest_decision

--- core/hpp/infrastructure/dbuct_nearest_decision.hpp
+++ core/hpp/infrastructure/dbuct_nearest_decision.hpp
@@ -1,6 +1,8 @@
 #ifndef DBUCT_NEAREST_DECISION_HPP
 #define DBUCT_NEAREST_DECISION_HPP
 
+#include <unordered_map>
+#include "debug_assert.hpp"
 #include "value_objects/lineage.hpp"
 
 template<typename ILogTrailAction>
@@ -11,6 +13,8 @@ struct dbuct_nearest_decision {
     void note_decision_resolution(const resolution_lineage* rl);
 
 private:
+    std::unordered_map<const resolution_lineage*, const resolution_lineage*> nd_;
+
     ILogTrailAction& trail_;
 };
 
@@ -18,9 +22,15 @@ template<typename ILogTrailAction>
 dbuct_nearest_decision<ILogTrailAction>::dbuct_nearest_decision(ILogTrailAction& t) : trail_(t) {}
 
 template<typename ILogTrailAction>
-void dbuct_nearest_decision<ILogTrailAction>::note_unit_resolution(const resolution_lineage* rl) {}
+void dbuct_nearest_decision<ILogTrailAction>::note_unit_resolution(const resolution_lineage* rl) {
+    auto [_, inserted] = nd_.insert({rl, nd_.at(rl->parent->parent)});
+    DEBUG_ASSERT(inserted);
+}
 
 template<typename ILogTrailAction>
-void dbuct_nearest_decision<ILogTrailAction>::note_decision_resolution(const resolution_lineage* rl) {}
+void dbuct_nearest_decision<ILogTrailAction>::note_decision_resolution(const resolution_lineage* rl) {
+    auto [_, inserted] = nd_.insert({rl, rl});
+    DEBUG_ASSERT(inserted);
+}
 
 #endif
